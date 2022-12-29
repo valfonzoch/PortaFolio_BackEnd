ffa21c1a821cae07ed36eceec21640bdca529195
valfonzoch <valfonzoch@gmail.com>
2022-12-29 07:41:50 -0300

    Primer commit backend

---
+++ src/test/java/com/miPrimeraApp/BackEnd/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miPrimeraApp.BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
